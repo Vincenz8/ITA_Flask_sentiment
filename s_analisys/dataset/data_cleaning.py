defdef28eaca88c92c62f9f9065df62145184342
Vincenz8 <vincenzo.russ8@outlook.com>
2021-08-26 16:25:31 +0200

    trad comment

--- s_analisys/dataset/data_cleaning.py
+++ s_analisys/dataset/data_cleaning.py
@@ -1,6 +1,7 @@
 # Vincenzo Russotto
 
-# librerie terze parti
+# 3rd party libraries
+
 import pandas as pd
 df = pd.read_csv("data_words.csv", delimiter= ";")
 df.drop(columns= ["N_definito1", "N_definito2", "Numero", "Numero2"], inplace= True)
